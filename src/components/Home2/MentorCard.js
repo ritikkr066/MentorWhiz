c1fe21d6bacacaf5a42a23b91b9c1c4b6cda099d
ritikkr066 <ritikkrsiwan345@gmail.com>
2024-04-17 17:55:13 +0530

    mentor-page

--- src/components/Home2/MentorCard.js
+++ src/components/Home2/MentorCard.js
@@ -5,12 +5,12 @@ import Link from "next/link";
 
 export default function MentorCard() {
 
-  const Column2 = ({ imageName, text, text2,text3 }) => (
+  const Column2 = ({ imageName, text, text2,text3,ctc}) => (
     <>
            
-      <div class="flex flex-col w-[360px] sm:w-fit    mb-4    m-0 text-gray-900   border-[2px] rounded-[20px]  mx-4 "> 
+      <div class="flex flex-col w-[350px] sm:w-fit    mb-4    m-0 text-gray-900   border-[2px] rounded-[20px] mx-0 md:mx-4 "> 
          <div className="flex "> <Image
-            width={360}
+            width={350}
             height={277}
             src={`/images/${imageName}`}
             alt=" random imgee"
@@ -21,10 +21,15 @@ export default function MentorCard() {
            <div class="mt-1 pt-4  text-[22px]  font-bold   justify-start flex  ">
                 {text}
               </div>
-              <div class=" text-[12px] text-[#4F4F4F] font-serif    justify-start flex  ">
+              <div class=" text-[14px] text-[#4F4F4F] font-normal     justify-start flex  ">
                 {text2}
               </div>
-               
+              <div class=" text-[12px] text-black font-normal    justify-start flex  ">
+                {text3}
+              </div>
+              <div class=" text-[12px] text-black font-normal    justify-start flex  ">
+                {ctc}
+              </div>
                
                 </div>
             </div>
@@ -36,15 +41,16 @@ export default function MentorCard() {
   return (
     <> 
      <div className="flex px-4  items-center sm:items-start sm:justify-start  justify-center text-[32px] font-serif font-bold py-6">Our Mentor</div>
+       <div className="px-2 sm:px-4">
         <div className="flex my-4 overflow-x-auto scrollbar-hide ">
       
         {/* <div className="flex flex-row "> */}
           <section className="">
             <Column2
-              imageName="man1.svg"
-              text="Aimee Calderon"
-              text2="B.Tech from NIT Patna"
-              text3="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt "
+              imageName="deep.jpg"
+              text="Deepanshu"
+              text2="BPCL , KANERIKA , SMARTCOIN "
+              text3="EX- Blaze AI"
             />
           </section>
           <section className="">
@@ -135,7 +141,7 @@ export default function MentorCard() {
               text3="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt "
             />
           </section>
-         
+          </div>
         </div>
         <div className="px-2 py-1 my-4 flex justify-center text-black  rounded-[8px] ">
                 <Link href={"/login"}>
